589b0eac33ee27e19fc168673570d5b41fb837a2
peanutnuts <ibuki.2.haga@gmail.com>
2025-11-30 21:46:09 +0900

    Edit test.bash

--- test.bash
+++ test.bash
@@ -2,16 +2,15 @@
 # SPDX-FIleCopyrightText: 2025 Ibuki Haga
 
 ng () {
-        echo "${1}行目が違うよ"
-        res=1
+    echo "${1}行目が違うよ"
+    res=1
 }
 
 res=0
 
-# 入力を与えて throwdis を実行
-out=$(printf "10\n45\n1\n0\n" | python3 throwdis)
+# throwdis を実行してプロンプト行を除外
+out=$(printf "10\n45\n1\n0\n" | python3 throwdis | grep -E "到達距離|落下時間|進行角度")
 
-# 期待される出力
 expected="到達距離          : 11.116 m
 落下時間          : 1.572 s
 落下時の進行角度  : -49.720 deg"
